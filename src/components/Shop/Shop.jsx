4f2cf0feada6fd1e0258b7f298cb69d3b44b8421
unknown <shefata69@gmail.com>
2023-03-29 21:47:51 +0600

    first commit

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Product from '../product/Product';
+import './Shop.css';
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, []);
+    useEffect( () =>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products])
+    const handleAddTocart = (product) =>{
+        // const newCart = [];
+        const newCart = [...cart, product];
+        // const exits = cart.find(pd => pd.id === product.id);
+        // if(!exits){
+        //     product.quantity = 1;
+        //     newCart = [...cart, product];
+        // }
+        // else{
+        //     exits.quantity = exits.quantity + 1;
+        //     const remaining = cart.filter(pd => pd.id !== product.id);
+        //     newCart = [...remaining, exits];
+        // }
+        setCart(newCart);
+        addToDb(product.id)
+    }
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product={product}
+                    handleAddTocart={handleAddTocart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+const Product = (props) => {
+    const { img, name, seller, quantity, price, ratings } = props.product;
+    const handleAddTocart = props.handleAddTocart;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Ratings: {ratings}</p>
+            </div>
+            <button onClick={() => handleAddTocart(props.product)} className='btn-cart'>Add To Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
+        </div>
+    );
+};
+
+export default Product;
